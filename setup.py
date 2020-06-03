99349212077f4084baf27d5b44cecd784d81b643
Victor Oguche <victoroguche@gmail.com>
2020-06-03 16:51:56 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name = "Distribution",
+setup(name="Distribution",
       version="0.1",
       description="Gaussian Distribution",
       packages=["Distributions"],
